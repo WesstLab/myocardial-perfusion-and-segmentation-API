f3cd05b84f9da25d160f9f9cd11d1750468ceb29
WesstLab <wesstlab@gmail.com>
2021-02-12 12:25:17 -0300

    Update colors.py

--- Interface/res/colors.py
+++ Interface/res/colors.py
@@ -1,22 +1,21 @@
-# Archivo para contener los colores que se utilizan en el programa
-
+# File to contain the colors to be used in the program
 # background = "#EDEDEC"
 #background = "#FFFFFF"
 background = "#f5f5f5"
 buton = "#0C0B0B"
 
-# Estandar de Paleta de Color
-CLR_1 = "#3c415e"#
-CLR_2 = "#738598"# alzul ggricaceo
-CLR_3 = "#dfe2e2"# blanco b es como el de los frames
-CLR_4 = "#1cb3c8" #Celeste
+# Color Palette Standard
+CLR_1 = "#3c415e"
+CLR_2 = "#738598"
+CLR_3 = "#dfe2e2"
+CLR_4 = "#1cb3c8" 
 
-# Paleta V2
-CLR_PL1 = "#00334e" # Azul oscuro
-CLR_PL2 = "#145374" #Azul mas menos oscuro
-CLR_PL3 = "#5588a3" #Azul mas menos claro
-CLR_PL4 = "#e8e8e8" # blanco c es emas parecido al de los prames
+# Palette V2
+CLR_PL1 = "#00334e" 
+CLR_PL2 = "#145374" 
+CLR_PL3 = "#5588a3"
+CLR_PL4 = "#e8e8e8"
 
 
-#  Color toque de imagen
-punto = "#BEF0DC" # celeste
+#  Color image touch
+punto = "#BEF0DC"
